bb03908403042eaf1178761ac408eefcca8a88fb
Tyler <tyler.dins88@gmail.com>
2023-04-16 21:54:22 -0700

    built models

---
+++ models/Thought.js
@@ -0,0 +1,61 @@
+const { Schema, model, Types } = require("mongoose");
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            require: true,
+            max_length: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                if (date) {
+                    return date.toISOString().split("T")[0];
+                }
+            }
+        },
+        username: {
+            type: String,
+            require: true
+        },
+        reactions: [
+            reactionSchema
+        ]
+    },
+    {
+        toJson: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+const reactionSchema = new Schema(
+    {
+        reactionID: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            require: true,
+            max_length: 280
+        },
+        username: {
+            type: String,
+            require: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false
+    }
+);
---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./Thought");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            require: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            require: true,
+            match: [/^([\w-.]+@([\w-]+.)+[\w-]{2,4})?$/, "Please use a valid email address."]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ]
+    },
+    {
+        toJSON: {
+            getters: true,
+        }
+    }
+);
+
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+})
+
+const User = model("user", userSchema);
+
+module.exports = User;
