e64f0a596201d80448e15e478521071cdd5bb46d
Tyler <tyler.dins88@gmail.com>
2023-04-17 15:25:54 -0700

    built userController for controllers

---
+++ controllers/userController.js
@@ -0,0 +1,119 @@
+const { ObjectId } = require("mongoose").Types;
+const { Thought, User } = require("../models");
+
+// Aggregate function to get the number of users overall
+// const headCount = async () =>
+//     User.aggregate()
+//         .count("userCount")
+//         .then((numberOfUsers) => numberOfUsers);
+
+module.exports = {
+    // get all users
+    getUsers(req, res) {
+        User.find()
+            .then(async (users) => {
+                const userObj = {
+                    users,
+                    // headCount: await headCount(),
+                };
+                return res.json(userObj);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+    // get a single student
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select("-__v")
+            .then(async (user) =>
+                !user
+                    ? res.status(404).json({ message: "No user with this ID :(" })
+                    : res.json({
+                        student
+                    })
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+    // create new user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true },
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "No user with this ID :(" })
+                    : res.json(user)
+            )
+            .catch((err) => res.json(500).json(err));
+    },
+    // delete a user and remove them from the social network
+    deleteUser(req, res) {
+        User.findOneAndRemove({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "No such user exists" })
+                    : User.findOneAndUpdate(
+                        { users: req.params.userId },
+                        { $pull: { users: req.params.userId } },
+                        { new: true }
+                    )
+            )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({
+                        message: "User deleted, but no thoughts found.",
+                    })
+                    : res.json({ message: "User completely deleted." })
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    // add thought to a user
+    addFriend(req, res) {
+        console.log("You have made a friend.");
+        console.log(req.body);
+        User.findOneAndUpdate(
+            { _id: req.params.friendId },
+            { $addToSet: { assignments: req.body } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res
+                        .status(404)
+                        .json({ message: "No user found with that ID :(" })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // remove thought from a user
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friend: { friendId: req.params.friendId } } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res
+                        .status(404)
+                        .json({ message: "No user found with that ID :(" })
+                    : res.json({ message: "You are no longer friends. " })
+            )
+            .catch((err) => res.status(500).json(err));
+    }
+}
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,3 +9,23 @@ const {
     removeReaction
 } = require("../../controllers/thoughtController");
 
+// /api/thoughts
+router.route("/")
+    .get(getThoughts)
+    .post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route("/api/:thoughtId")
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reaction
+router.route("/api/:thoughtId/reaction")
+    .post(createReaction);
+
+// /api/thoughts/:thoughtId/reaction/:reactionId
+router.route("/api/:thoughtId/reaction/:reactionId")
+    .delete(removeReaction);
+
+module.exports = router;
